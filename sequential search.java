a4699694bbbd2a5400ef3f006b2209f521c7cee0
Velavanvel9 <velavanvel965@gmail.com>
2024-09-11 10:02:43 +0530

    Add files via upload

---
+++ sequential search.java
@@ -0,0 +1,26 @@
+class linear{
+public static int search(int arr[],int x)
+{ 
+int n=arr.length;
+for(int i=0;i<n;i++){
+if(arr[i]==x)
+return i;
+}
+return -1;
+}
+ public static void main(string args[])
+{
+int arr[]={2,3,4,10,40};
+int x=10;
+int result=search(arr,x);          
+if(result==-1)
+system.out.print("element is not present in array");
+else
+system.out.print("element is present"+"at intex"+result);
+}
+}
+
+
+
+
+
